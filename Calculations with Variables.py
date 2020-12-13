b31b506db0d8da41bbf3c4d8c0301a937c4c50f9
jessennis <jessica.enni@hse.ie>
2020-12-13 21:39:36 +0000

    commit 9

---
+++ Calculations with Variables.py
@@ -0,0 +1,15 @@
+Create a variable growth_multiplier, equal to 1.1.
+Create a variable, result, equal to the amount of money you saved after 7 years.
+Print out the value of result.
+
+# Create a variable savings
+savings = 100
+print(savings)
+# Create a variable growth_multiplier
+growth_multiplier=1.1
+print(growth_multiplier)
+# Calculate result
+result = (100*1.1**7)
+print(result)
+# Print out result
+print((savings)*(growth_multiplier))
---
+++ Python as a calculator.py
@@ -0,0 +1,13 @@
+Suppose you have $100, which you can invest with a 10% return each year. After one year, it's  dollars, and after two years it's . Add code to calculate how much money you end up with after 7 years, and print the result.
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+print(100 * 1.1 ** 7)
+
+# How much is your $100 worth after 7 years?
