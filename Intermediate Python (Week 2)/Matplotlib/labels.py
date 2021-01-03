363690b458ec6d1e42ff0dddd6a53558738e0d42
jessennis <jessica.enni@hse.ie>
2021-01-03 15:11:44 +0000

    commit 20

---
+++ Intermediate Python (Week 2)/Matplotlib/labels.py
@@ -0,0 +1,25 @@
+It's time to customize your own plot. This is the fun part, you will see your plot come to life!
+
+You're going to work on the scatter plot with world development data: GDP per capita on the x-axis (logarithmic scale), life expectancy on the y-axis. The code for this plot is available in the script.
+
+As a first step, let's add axis labels and a title to the plot. You can do this with the xlabel(), ylabel() and title() functions, available in matplotlib.pyplot. This sub-package is already imported as plt.
+
+# Basic scatter plot, log scale
+plt.scatter(gdp_cap, life_exp)
+plt.xscale('log')
+
+# Strings
+xlab = "GDP per Capita [in USD]"
+ylab = "Life Expectancy [in years]"
+title = "World Development in 2007"
+
+# Add axis labels
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+
+
+# Add title
+plt.title('World Development in 2007')
+
+# After customizing, display the plot
+plt.show()
